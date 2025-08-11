2bf9df5b258d7860214c443b791d433e525aec49
CmrCrabs <zayaanazam1234@hotmail.com>
2025-08-11 12:04:48 +0100

    initial for icarus

--- ags/consts.js
+++
@@ -1,7 +0,0 @@
-export const consts = {
-    wallpaper_directory: "~/limitless/wallpapers",
-    dotfiles_directory: "~/limitless",
-    template_directory: "~/limitless/home/templates",
-    monitor_0: "eDP-1",
-    monitor_1: "DP-1",
-};
