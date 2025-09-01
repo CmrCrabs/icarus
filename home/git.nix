ce2843e140e354e5f7e4a5673662d1f74fcc141c
CmrCrabs <zf.azam@proton.me>
2025-09-01 17:06:41 +0100

    darwin initial + nvim

--- home/git.nix
+++ home/git.nix
@@ -1,6 +1,7 @@
 {
  lib,
  config,
+ pkgs,
  ...
 }: let
   email = "zf.azam@proton.me";
@@ -10,6 +11,9 @@ in {
   options.git.enable = lib.mkEnableOption "enables git";
 
   config = lib.mkIf config.git.enable {
+    home.packages = [
+      pkgs.gh
+    ];
     programs.git = {
       enable = true;
       userEmail = email;
